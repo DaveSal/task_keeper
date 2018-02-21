9d4684c98890b6a8f0389ccf1c5d3511a2d2cf2b
dsalamau <salamaudavid@gmail.com>
2018-02-21 14:52:43 +0600

    Add api logic

---
+++ app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,25 @@
+class API::V1::NotesController < API::V1::BaseController
+  def index
+    respond_with Note.all
+  end
+
+  def create
+    respond_with :api, :v1, Note.create(notes_params)
+  end
+
+  def update
+    note = Note.find(params['id'])
+    note.update_attributes(notes_params)
+    respond_with note
+  end
+
+  def destroy
+    respond_with Note.destroy(params['id'])
+  end
+
+  private
+
+  def notes_params
+    params.require(:note).permit(:id, :title, :description)
+  end
+end
